e429bdc9093eb59999f8be244e72b88991eddcd8
RAM <ram.tamang2058@gmail.com>
2022-11-09 17:48:29 +0545

    selection sorting algorithm simple steps

---
+++ selection sort.py
@@ -0,0 +1,23 @@
+#selection sort is algorithm used to sorting the array from lower to higer or higer to lower
+#sort
+
+import sys
+A = [64, 25, 12, 32, 78, 56]
+
+#traverse through all array element
+
+for i in range(len(A)):
+    #lets find the minimum element exist in array
+    min_idx = i
+    for j in range(i+1, len(A)):
+        if A[min_idx]>A[j]:
+            min_idx = j
+    #swap the found minimum element with the first element
+    A[i], A[min_idx] = A[min_idx], A[i]
+
+print("sorted array are:")
+
+for i in range(len(A)):
+    print("%d" %A[i], end=" ")
+
+print("\n")
